779ebf9173706c7bcadf1fe304392a6bf125cdbe
Darkemberg <darkemberg@gmail.com>
2018-07-30 22:08:21 -0500

    SMS Server: Android part. Start.

---
+++ app/src/main/java/com/mutuofinanciera/smsserver/service/SMSService.java
@@ -0,0 +1,170 @@
+package com.mutuofinanciera.smsserver.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.telephony.SmsManager;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class SMSService extends Service {
+
+    public static final int PORT = 3512;
+    public static final String SMS_BODY_TAG = "sms_body";
+    public static final String SMS_URI_FORMAT = "smsto: %s";
+
+    private boolean run;
+
+    private int mSMSCounter;
+
+    private ServerSocket mSocketServer;
+
+    private Thread mThread;
+
+    public SMSService() {
+        mSMSCounter = 0;
+
+        run = true;
+
+        mThread = new Thread(new Runnable() {
+            public void run() {
+                while (run) {
+                    readMessage();
+                }
+            }
+        });
+    }
+
+    private void readMessage() {
+        Socket client = acceptClient();
+
+        if (client != null) {
+            sendData(client, mSMSCounter);
+
+            String number = receiveData(client);
+            String message = receiveData(client);
+
+            sendMessage(number, message);
+        }
+
+        closeClient(client);
+    }
+
+    private String receiveData(Socket client) {
+        String result = "";
+        try {
+            BufferedReader inputStream = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            result = inputStream.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result;
+    }
+
+    private void closeClient(Socket client) {
+        try {
+
+            client.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendData(Socket client, int data) {
+        PrintStream outputStream;
+        try {
+
+            outputStream = new PrintStream(client.getOutputStream());
+            outputStream.println(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void sendData(Socket client, String data) {
+        PrintStream outputStream;
+        try {
+
+            outputStream = new PrintStream(client.getOutputStream());
+            outputStream.println(data);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Socket acceptClient() {
+        Socket client = null;
+
+        try {
+
+            client = mSocketServer.accept();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return client;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        createServer(PORT);
+
+        mThread.start();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        stopServer();
+    }
+
+    private void createServer(int port) {
+
+        try {
+
+            mSocketServer = new ServerSocket(port);
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    private void stopServer() {
+
+        try {
+            run = false;
+            mSocketServer.close();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    private void sendMessage(String destinationAddress, String message) {
+
+        SmsManager smsManager = SmsManager.getDefault();
+
+        smsManager.sendTextMessage(destinationAddress, null, message,
+                null, null);
+
+        mSMSCounter++;
+    }
+}
